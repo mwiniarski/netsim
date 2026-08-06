5d70c62009357f91bc1f6311cbcddc16c895c71f
mwiniarski <mw.pari@gmail.com>
2026-08-06 22:03:03 +0200

    fix

--- src/main.rs
+++ src/main.rs
@@ -9,18 +9,11 @@ fn main() -> eframe::Result {
     let native_options = eframe::NativeOptions {
         viewport: egui::ViewportBuilder::default()
             .with_inner_size([400.0, 300.0])
-            .with_min_inner_size([300.0, 220.0])
-            .with_icon(
-                // NOTE: Adding an icon is optional
-                eframe::icon_data::from_png_bytes(
-                    &include_bytes!("../assets/favicon-512x512.png")[..],
-                )
-                .expect("Failed to load icon"),
-            ),
+            .with_min_inner_size([300.0, 220.0]),
         ..Default::default()
     };
     eframe::run_native(
-        "eframe template",
+        "NetSim",
         native_options,
         Box::new(|cc| Ok(Box::new(net_sim::App::new(cc, net_sim::network::network())))),
     )
